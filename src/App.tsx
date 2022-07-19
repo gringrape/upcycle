2d021835fe5ba50b6a4dcd5edfc695cfaf387ecc
YE H <canary4651@gmail.com>
2022-07-19 17:27:05 +0900

    기본 디자인을 설정한다
    Reset styles with “Eric Meyer's Reset CSS”.
    
    - [CSS Tools: Reset CSS](https://meyerweb.com/eric/tools/css/reset/)
    - [styled-reset](https://github.com/zacanger/styled-reset)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 
+import styled from 'styled-components';
+
+import { colors } from './designSystem';
+
 import users from '../data/users';
 
 import random from '../utils/random';
@@ -8,6 +12,39 @@ type User = {
     id: number;
     name: string;
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 2em;
+  font-weight: 500;
+  margin: 1em 0;
+`;
+
+const List = styled.ul`
+`;
+
+const Item = styled.li`
+  margin-block: .5em; 
+  padding: .5em 5em;
+  text-align: center;
+`;
+
+const Button = styled.button`
+  font-size: 1.1em;
+  text-align: center;
+  margin: 2em;
+  padding: 1em;
+  width: 70%;
+  border: 0px solid;
+  border-radius: 3px;
+  color: ${colors.white};
+  background-color: ${colors.primary};
+`;
 
 export default function App() {
   const [winners, pick] = useState<User[]>([]);
@@ -23,18 +60,18 @@ export default function App() {
   };
 
   return (
-    <div>
-      <h1>분리수거 가챠</h1>
-      <ul>
+    <Container>
+      <Title>분리수거 가챠 🎱</Title>
+      <List>
         {users.map((user) => (
-          <li key={user.id}>
+          <Item key={user.id}>
             {user.name}
-          </li>
+          </Item>
         ))}
-      </ul>
-      <button type="button" onClick={handleClick}>
-        pick
-      </button>
+      </List>
+      <Button type="button" onClick={handleClick}>
+        PICK ME UP 🎰
+      </Button>
       {winners.length ? (
         <p>
           축하합니다!
@@ -45,6 +82,6 @@ export default function App() {
           😜
         </p>
       ) : null}
-    </div>
+    </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import { createRoot } from 'react-dom/client';
 
+import { Reset } from 'styled-reset';
+
+import GlobalStyle from './GlobalStyle';
 import App from './App';
 
 const container = document.getElementById('app');
@@ -8,6 +12,8 @@ const container = document.getElementById('app');
 const root = createRoot(container!);
 root.render((
   <React.StrictMode>
+    <Reset />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 ));
